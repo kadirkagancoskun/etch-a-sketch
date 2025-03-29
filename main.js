5f853970210f36fcf43881f329c81e1bd6b250a0
kadirkagancoskun <kadirkagancoskun@gamil.com>
2025-03-29 05:42:02 +0100

    Add grid, hover effects with bw and rgb

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
-alert("bö")
 const container = document.querySelector("#container");
+const sketchboard = document.querySelector(".sketchboard");
 
-const content = document.createElement("div");
-content.classList.add("content");
+let colorChoice;
 
-content.textContent="bööö";
-for (let i = 0; i<=16; i++){
-    for(let j=0; j<=16; j++){
-    let content = document.createElement("div");
-    content.classList.add("content");
-    
-    content.textContent="bööö";
-    container.appendChild(content);
-    }
+function changeColorChoice(colorBtn){
+    colorChoice = colorBtn;
 }
+
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+let color = random_rgba();
+
+function clearGrid(container){
+   container.innerHTML = ""
+}
+
+
+
+function createGrid(){
+    clearGrid(container)
+    
+    let gridbool = true;
+    let gridSize;
+    loop:
+    while(true){
+        gridSize = Number(prompt("Give a number between 1-100", "16"));
+        if(gridSize>1 & gridSize<100){
+            for (let i = 0; i<=gridSize-1; i++){
+                for(let j=0; j<=gridSize-1; j++){
+                    const content = document.createElement("div");
+                    let contentSize = 600 / gridSize;
+                    content.classList.add("content");
+                    container.appendChild(content);
+        
+                    content.style.width=`${contentSize}px`;
+                    content.style.height=`${contentSize}px`;
+            
+            content.addEventListener("mouseover", (e)=> {
+                if(colorChoice=="bw"){
+                    e.target.style.backgroundColor=`black`;
+                } else{
+                e.target.style.backgroundColor=`${random_rgba()}`;
+                }
+            })
+                
+                
+                }
+            }
+            gridbool=false;
+            break loop;  
+        }else{
+            alert("Enter a valid number!");
+        }
+    }
+
+    
+
+}
+
+
